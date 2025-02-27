9d744dc2f0257d7578249b9fea0dcbe9c9873532
Emirhan <emirhanbuluttech@gmail.com>
2025-02-26 23:18:47 -0700

    New Template for Generation Manager

--- Assets/Scripts/GenerationManager.cs
+++ Assets/Scripts/GenerationManager.cs
@@ -1,6 +1,39 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GenerationManager : MonoBehaviour
 {
     //General Script Manager for Generation Classes
+
+    [Header("Generation Prefabs")]
+    public GameObject[] platformPrefabs;
+    public GameObject winZone;
+    [SerializeField] private Transform borderTransform; 
+
+    [Header("Generation Variables")]
+    public int platformPerCycle;
+    [SerializeField] private float minY_DistanceBetweenPlatform = 1.5f; 
+    [SerializeField] private float maxY_DistanceBetweenPlatform = 3.5f;
+
+    [Tooltip("The starting Y Vector for platform generation")]
+    [SerializeField] private float startY = 3.5f;
+    private float lastPlatformPosition; 
+
+    
+    public void GeneratePlatforms()
+    {
+        //fail Check
+        if(platformPrefabs.Length == 0){Debug.LogWarning("Platform prefab count on prefab array = 0"); return;}
+        
+        for (int i = 0; i < platformPerCycle; i++)
+        {
+            GameObject randomPlatform = RandomlyGeneratedPlatform();
+        }
+
+    }
+
+    private GameObject RandomlyGeneratedPlatform()
+    {
+        return null; 
+    }
 }
