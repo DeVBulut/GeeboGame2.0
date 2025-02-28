e7bc34c5b0613a2e3d19598004745290e2781caa
Emirhan <emirhanbuluttech@gmail.com>
2025-02-27 23:13:54 -0700

    Update GenerationManager
    Generation Manager Still needs work. The base architecture for it is there but the variables seem to be bugging out randomly will work on it tomorow.

--- Assets/Scripts/GenerationManager.cs
+++ Assets/Scripts/GenerationManager.cs
@@ -8,29 +8,53 @@ public class GenerationManager : MonoBehaviour
     [Header("Generation Prefabs")]
     public GameObject[] platformPrefabs;
     public GameObject winZone;
-    [SerializeField] private Transform borderTransform; 
+    [SerializeField] public Transform borderTransform; 
 
     [Header("Generation Variables")]
-    public int platformPerCycle;
     [SerializeField] private float minY_DistanceBetweenPlatform = 1.5f; 
     [SerializeField] private float maxY_DistanceBetweenPlatform = 3.5f;
 
     [Tooltip("The starting Y Vector for platform generation")]
-    [SerializeField] private float startY = 3.5f;
-    private GameObject lastPlatform; 
+    [SerializeField] private float currentPlatformY;
+    private GameObject lastPlatform;
 
-    
+    public int platformPerCycle = 200; 
+    public float averageSpawnInterval = 0.5f;
+    public float spawnIntervalChange = 1f;
+
+
+
+
+    private void Start()
+    {
+        //GeneratePlatforms(250, 1, 2);
+    }
+
     public void GeneratePlatforms()
     {
         //fail Check
         if(platformPrefabs.Length == 0){Debug.LogWarning("Platform prefab count on prefab array = 0"); return;}
         
+        //Type selection and position loop.
         for (int i = 0; i < platformPerCycle; i++)
         {
             GameObject randomPlatform = RandomlyGeneratedPlatform();
             lastPlatform = randomPlatform;
-        }
+            Vector3 spawnLocation = SetPosition(averageSpawnInterval, spawnIntervalChange);
 
+            Instantiate(randomPlatform, spawnLocation, Quaternion.identity); 
+        }
+    }
+
+    private Vector3 SetPosition(float averageSpawnInterval, float spawnIntervalChange)
+    {
+        float yPosition = lastPlatform.transform.position.y; 
+
+        float xSpawnPosition = Random.Range(-borderTransform.position.x, borderTransform.position.x);
+        float ySpawnPosition = Random.Range(yPosition + averageSpawnInterval,  yPosition + averageSpawnInterval + spawnIntervalChange);
+        
+        Vector3 spawnLocation = new Vector3(xSpawnPosition, yPosition, 0);
+        return spawnLocation;
     }
 
     private GameObject RandomlyGeneratedPlatform()
@@ -56,5 +80,5 @@ public class GenerationManager : MonoBehaviour
 
         Debug.LogWarning("Unintended Behaviour Blocker - " + platformPrefabs[0].name + " is returned");
         return platformPrefabs[0]; // Fallback (should never happen)
-    }
+    }    
 }
